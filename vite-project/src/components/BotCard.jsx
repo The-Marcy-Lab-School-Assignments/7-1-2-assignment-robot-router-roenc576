e3accba1ec528b6d0454c64f0a9a2550d4c06132
roenc576 <roenc576@gmail.com>
2024-08-11 12:35:15 -0400

    Used useNavigate hook instead of Link to navigate programatically

--- vite-project/src/components/BotCard.jsx
+++ vite-project/src/components/BotCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const BotClassIcon = (bot_class) => {
   switch (bot_class) {
@@ -17,46 +18,47 @@ const BotClassIcon = (bot_class) => {
 // - Update the onClick handler such that clicking on a bot card will 
 // navigate the user to /robots/:id where :id is the id of the current bot
 // - Do NOT use a <Link> component to accomplish this
+// USE USENAVIGATE HOOK INSTEAD
+
 
 const BotCard = ({ bot }) => {
 
-  const handleClick = (e) => {
-    console.log(bot);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/robots/${bot.id}`);
     console.log(`take me to robot ${bot.id}`);
-    console.log(e.target);
   }
 
   return (
     <div className="ui card" onClick={handleClick}>
-      <Link to={`/robots/${bot.id}`}>
-        <div className="image">
-          <img alt={bot.name} src={bot.avatar_url} />
+      <div className="image">
+        <img alt={bot.name} src={bot.avatar_url} />
+      </div>
+      <div className="content">
+        <div className="header">
+          {bot.name} {BotClassIcon(bot.bot_class)}
         </div>
-        <div className="content">
-          <div className="header">
-            {bot.name} {BotClassIcon(bot.bot_class)}
-          </div>
 
-          <div className="meta">
-            <small>{bot.catchphrase}</small>
-          </div>
+        <div className="meta">
+          <small>{bot.catchphrase}</small>
         </div>
-        <div className="extra content">
-          <span>
-            <i className="icon heartbeat" />
-            {bot.health}
-          </span>
+      </div>
+      <div className="extra content">
+        <span>
+          <i className="icon heartbeat" />
+          {bot.health}
+        </span>
 
-          <span>
-            <i className="icon lightning" />
-            {bot.damage}
-          </span>
-          <span>
-            <i className="icon shield" />
-            {bot.armor}
-          </span>
-        </div>
-      </Link>
+        <span>
+          <i className="icon lightning" />
+          {bot.damage}
+        </span>
+        <span>
+          <i className="icon shield" />
+          {bot.armor}
+        </span>
+      </div>
     </div>
   );
 }
